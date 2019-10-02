26e9736fd3909b07d7b1829aa94baa2d894097f5
Bruno Cesar <bruunoocesar@gmail.com>
2019-10-02 20:32:12 -0300

    Mudei a extensão do index para '.php' e inclui os elementos comuns (navbar, rodapé, includes) usando a função include() do PHP.

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <?php include('layoutTagHead.php') ?>
+
+    <body>
+        <?php include('layoutNavbar.php') ?>
+
+        <main class="container-fluid" role="main">
+            <div class="container">
+                <div class="row page-row">
+                    <div class="col-0 col-md-2 col-lg-2 col-xl-2 py-md-3 pl-md-5 bd-content"></div>
+                    <div class="col-12 col-md-8 col-lg-8 col-xl-8 py-md-3 pl-md-5 bd-content page-content">
+                        <h1>Título</h1>
+                        <p>Paragrafozinho de descrição de página.</p>
+                    </div>
+                    <div class="col-0 col-md-2 col-lg-2 col-xl-2 py-md-3 pl-md-5 bd-content"></div>
+                </div>
+            </div>
+        </main>
+
+        <?php include('layoutFooter.php') ?>
+        
+        
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    </body>
+</html>
---
+++ layoutTagHead.php
@@ -0,0 +1,10 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style/style.css">
+    <link rel="shortcut icon" href="style/brand/brand_favicon.ico" type="image/x-icon" />
+    <link href="style/icons/css/all.css" rel="stylesheet">
+    
+    <title>@brnocesar</title>
+</head>
