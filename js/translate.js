30607d7693c071beeeb5c5dd9501a7677e9c2d48
Bruno Cesar <bruunoocesar@gmail.com>
2020-05-10 12:41:55 -0300

    Implementa switch de idiomas em função da tag 'lang'

--- js/translate.js
+++ js/translate.js
@@ -1,19 +1,24 @@
 function translate(language, status){
-    var elements = document.getElementsByClassName("lang-" + language);
+    var elements = document.querySelectorAll(`[lang=${language}]:not(html)`);
+    
     for(var i=0; i<elements.length; i++) {
         elements[i].hidden = status;
     }
 }
 
 function toEnglish(){
+    // alert('toEnglish')
     translate("pt", true)
     translate("en", false)
 }
 
 function toPortuguese(){
+    // alert('toPortuguese')
     translate("en", true)
     translate("pt", false)
 }
 
-// se for usar tag 'lang'
-// $("[lang='pt']")
+// proxima mudança:
+// - metodo apenas para trocar o valor de 'lang'
+// - metodo que busca num JSON o texto que deve ser aplicado de acordo com o valor de 'lang'
+// - armazenar na sessao o ultimo valor de 'lang', roda o metodo acima ao carregar também
